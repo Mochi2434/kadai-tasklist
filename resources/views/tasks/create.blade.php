8eb840002aebb321968d77facab4404559803d0b
Mochi2434 <mochi2434@gmail.com>
2019-11-01 18:27:53 +0000

    tentative

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク新規作成ページ</h1>
+    
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                {!! Form::submit('作成') !!}
+            
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+@endsection
